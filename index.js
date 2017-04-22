77da69ca50fb77524c6dce649a2419a0febd648d
Gurkirat Singh <tablagurkirat@yahoo.com>
2017-04-22 16:12:16 -0700

    refactored frameworks

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -14,9 +15,17 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 
 app.get('/', function (req, res) {
-  res.redirect('index.html');
+  res.sendFile(path.join(__dirname,'index.html'));
 })
 
+app.get('/phone', function (req, res) {
+  res.sendFile(path.join(__dirname,'phone.html'));
+})
+
+app.use('/css', express.static(path.join(__dirname, '/css')));
+app.use('/js', express.static(path.join(__dirname, '/js')));
+app.use('/images', express.static(path.join(__dirname, '/images')));
+app.use('/fonts', express.static(path.join(__dirname, '/fonts')));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
