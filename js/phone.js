c6889d104cab1cde094c02360e4960551ff52301
Ziqing <zack.ziqing.guo@gmail.com>
2017-04-22 17:10:04 -0700

    test cases added

--- js/phone.js
+++ js/phone.js
@@ -1,3 +1,5 @@
+//var exports = module.exports = {};
+
 var re = new RegExp(String.raw`^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$`);
 
 function isValidPhoneNumber(strNumToCheck) {
@@ -7,6 +9,8 @@ function isValidPhoneNumber(strNumToCheck) {
 	}
 	return false;
 }
+
+
 function setFormat() {
 	var v = document.getElementById('phoneformat').value;
 	var telNumber = document.getElementById('telnumber').value;
@@ -15,4 +19,9 @@ function setFormat() {
 		re = new RegExp(String.raw`${v}`);
 	}
 	alert(isValidPhoneNumber(telNumber));
+}
+
+module.exports = {
+	isValidPhoneNumber: isValidPhoneNumber,
+	setFormat: setFormat
 }
