5ce28c530e781b8edd9abd8b5d46b58112cc82c3
Gurkirat Singh <tablagurkirat@yahoo.com>
2017-04-25 01:42:19 -0700

    regex changes

--- js/phone.js
+++ js/phone.js
@@ -1,27 +1,47 @@
-//var exports = module.exports = {};
+function CheckPhoneNum(){
+	var format_elem = document.getElementById('phoneformat').value;
+	var telnumber = document.getElementById('telnumber').value;
+	console.log(exports);
+	regex = setFormat(format_elem, telnumber);
+	alert(isValidPhoneNumber(telnumber,regex));
+}
 
-var re = new RegExp(String.raw`^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$`);
-
-function isValidPhoneNumber(strNumToCheck) {
-	var matches = strNumToCheck.match(re);
-	if (matches) {
-		return true;
-	}
+function isValidPhoneNumber(strNumToCheck,regex) {
+	var matches = strNumToCheck.match(regex);
+	if (matches) return true;
 	return false;
 }
 
 
-function setFormat() {
-	var v = document.getElementById('phoneformat').value;
-	var telNumber = document.getElementById('telnumber').value;
-	//var re = new RegExp(String.raw`^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$`);
-	if (v != null && v !="") {
-		re = new RegExp(String.raw`${v}`);
+function setFormat(pformat,telnumber) {
+	var regex = new RegExp(
+		String.raw`^((\([0-9]{3}\))|([0-9]{3}))[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$`);
+
+	//if format is null, then use default regex
+	if (pformat != null && pformat !="") {
+		var new_regex = "^";
+		for (var i in pformat) {
+			char = pformat[i];
+			if (isDigit(char)){
+				new_regex += "[0-9]";
+			}else{
+				new_regex += ('\\' + char);
+			}
+		}
+		new_regex += "$";
+		regex = new RegExp(String.raw`${new_regex}`);
 	}
-	alert(isValidPhoneNumber(telNumber));
+	return regex;
 }
 
-module.exports = {
-	isValidPhoneNumber: isValidPhoneNumber,
-	setFormat: setFormat
+//helper function to check if the given character is a digit
+function isDigit(digit_str){
+	return digit_str.match(/[0-9]/) != null;
 }
+
+
+module.exports = {
+	isValidPhoneNumber : isValidPhoneNumber,
+	setFormat : setFormat
+}
+
